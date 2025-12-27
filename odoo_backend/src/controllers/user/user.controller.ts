1fbec831df8b0d3c977b1d773141d8ca3493eaa3
parmar krish <parmarkrish005@gmail.com>
2025-12-27 11:19:20 +0530

    add into the new branch

---
+++ odoo_backend/src/controllers/user/user.controller.ts
@@ -0,0 +1,11 @@
+import {operate} from "../../utils/Opreation.js";
+import { UserModel } from "../../models/user.models.js";
+import { asyncHandler } from "../../utils/asyncHandler.js";
+import type { Request , Response } from "express";
+
+const createRequest = asyncHandler(async(req,res)=>{
+
+    
+    
+})
+
